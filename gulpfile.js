d7dce6aa87597d53f0678226ac454228855e7ada
mac.grewal <mac.grewal@hotmail.com>
2016-11-24 23:06:08 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./assets/css/style.scss')
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./assets/css/*.scss', ['sass']);
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'app.js',
+    ext: 'js coffee nunjucks',
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if (/^Express server listening on port/.test(chunk)) {
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
+
+gulp.task('default', [
+  'sass',
+  'develop',
+  'watch'
+]);
