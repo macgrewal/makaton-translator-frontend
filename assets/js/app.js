5eacf3d8136af49f92c2fb5956cd3fc8c579133f
mac.grewal <mac.grewal@hotmail.com>
2016-12-03 19:09:16 +0000

    simple view switching

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,93 +1,111 @@
 $(document).ready(function () {
-  var englishForm = $('form[data-language="english"]'),
-    makatonForm = $('form[data-language="makaton"]'),
-    toEnglishButton = $('button', makatonForm),
-    toMakatonButton = $('button', englishForm),
-    englishUsername = englishForm.data('username'),
-    makatonUsername = makatonForm.data('username'),
-    toMakatonButton = $('button', englishForm),
-    words = $('[name="words"]', englishForm),
-    cards = [],
-    makaton = $('#makaton'),
-    currentCard = $('#coreWords'),
-    add = $('#add-card'),
-    remove = $('#remove-card'),
+  var englishView = $('form[data-language="english"]'),
+    back = $('a[href="#makaton-display"]'),
+    // makatonCardsTranslation = $('#'),
+    // makatonWordsTranslation = $('#'),
+    // english = $('#'),
+    reply = $('#translate-to-makaton'),
+
+    makatonView = $('form[data-language="makaton"]'),
+    // makatonCardsContainer = $('#'),
+    // removeLastCard = $('#'),
+    translateToEnglish = $('#translate-to-english'),
+    // makatonCardCategories = $('#'),
+
     coreVocab = [];
 
   // stop the buttons from posting back to the server
-  $('form[data-language] > button').click(function (e) {
+  $('button').click(function (e) {
     e.preventDefault();
   });
 
-  toEnglishButton.click(function () {
-    $.ajax({
-      url: '/api/words',
-      data: {
-        username: englishUsername,
-        cards: cards
-      },
-      dataType: 'json',
-      success: function (data) {
-        words.val(data.words);
-        var utterance = new SpeechSynthesisUtterance(data.words);
-        utterance.lang = 'en-GB';
-        window.speechSynthesis.speak(utterance);
-      }
-    });
-  });
-
-  toMakatonButton.click(function () {
-    $.ajax({
-      url: '/api/cards',
-      data: {
-        username: englishUsername,
-        words: words.val()
-      },
-      dataType: 'json',
-      success: setCards
-    });
-  });
-
-  add.click(function () {
-    addCard(currentCard.val());
-  });
-
-  remove.click(function () {
-    cards.pop();
-    $('img:last', makaton).remove();
-  });
-
-  function addCard(cardId) {
-    var image = '<img src="/img/core/' + cardId + '.png" />';
-    makaton.append(image);
-    cards.push({
-      id: cardId
-    });
+  function switchViews() {
+    englishView.toggle();
+    makatonView.toggle();
   }
 
-  function clearCards() {
-    cards = [];
-    makaton.empty();
+  function setupPage() {
+    englishView.hide();
+    makatonView.show();
   }
 
-  function setCards(data) {
-    clearCards();
-    var cards = data.cards;
-    for (i = 0; i < cards.length; i++) {
-      addCard(cards[i].id);
-    }
-  }
+  back.click(switchViews);
+  reply.click(switchViews);
+  translateToEnglish.click(switchViews);
 
-  function loadCoreVocabulary() {
-    $.getJSON('/js/core-vocab.json', function (words) {
-      for (var i = 0; i < words.length; i++) {
-        var word = words[i];
-        var option = '<option value="' + word.id + '">' + word.word + '</option>';
-        currentCard.append(option);
-      }
-      coreVocab = words;
-    });
-  }
+  setupPage();
 
-  loadCoreVocabulary();
+
+
+  // toEnglishButton.click(function () {
+  //   $.ajax({
+  //     url: '/api/words',
+  //     data: {
+  //       username: englishUsername,
+  //       cards: cards
+  //     },
+  //     dataType: 'json',
+  //     success: function (data) {
+  //       words.val(data.words);
+  //       var utterance = new SpeechSynthesisUtterance(data.words);
+  //       utterance.lang = 'en-GB';
+  //       window.speechSynthesis.speak(utterance);
+  //     }
+  //   });
+  // });
+
+  // toMakatonButton.click(function () {
+  //   $.ajax({
+  //     url: '/api/cards',
+  //     data: {
+  //       username: englishUsername,
+  //       words: words.val()
+  //     },
+  //     dataType: 'json',
+  //     success: setCards
+  //   });
+  // });
+
+  // add.click(function () {
+  //   addCard(currentCard.val());
+  // });
+
+  // remove.click(function () {
+  //   cards.pop();
+  //   $('img:last', makaton).remove();
+  // });
+
+  // function addCard(cardId) {
+  //   var image = '<img src="/img/core/' + cardId + '.png" />';
+  //   makaton.append(image);
+  //   cards.push({
+  //     id: cardId
+  //   });
+  // }
+
+  // function clearAllCards() {
+  //   cards = [];
+  //   makaton.empty();
+  // }
+
+  // function setCards(data) {
+  //   clearAllCards();
+  //   var cards = data.cards;
+  //   for (i = 0; i < cards.length; i++) {
+  //     addCard(cards[i].id);
+  //   }
+  // }
+
+  // function loadCoreVocabulary() {
+  //   $.getJSON('/js/core-vocab.json', function (words) {
+  //     for (var i = 0; i < words.length; i++) {
+  //       var word = words[i];
+  //       var option = '<option value="' + word.id + '">' + word.word + '</option>';
+  //       currentCard.append(option);
+  //     }
+  //     coreVocab = words;
+  //   });
+  // }
+
+  // loadCoreVocabulary();
 });
